78e7c2181a009dfa2fb62949217146c3ee410e87
47474330 <aydengebran@icloud.com>
2020-10-08 22:11:46 +1100

    - Added parallax scrolling to the hero image transitioning into the image in the design section - Added state variables to the TypeScript code to boost performance by avoiding changing DOM elements with jQuery unless they have been changed - The material previews now drift closer together when scrolling past them

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,3 +1,5 @@
+let activeSection = ``;
+let heroImageIsVisible = true;
 const main = () => {
     setGlobalListeners();
 };
@@ -6,20 +8,62 @@ const setGlobalListeners = () => {
 };
 const didScroll = () => {
     updateActiveSection();
+    updateHeroImage();
+    updateMaterialPositions();
 };
 const updateActiveSection = () => {
     const scrollY = $(window).scrollTop();
-    let activeSection = ``;
+    let section = ``;
     if (scrollY > $(`#gallery`).offset().top)
-        activeSection = `gallery`;
+        section = `gallery`;
     else if (scrollY > $(`#houses`).offset().top)
-        activeSection = `houses`;
+        section = `houses`;
     else if (scrollY > $(`#materials`).offset().top)
-        activeSection = `materials`;
+        section = `materials`;
     else if (scrollY > $(`#design`).offset().top)
-        activeSection = `design`;
-    $(`header nav li`).removeClass(`active`);
-    if (activeSection != ``)
-        $(`header nav li#${activeSection}-link`).addClass(`active`);
+        section = `design`;
+    if (section != activeSection) {
+        $(`header nav li`).removeClass(`active`);
+        if (section != ``)
+            $(`header nav li#${section}-link`).addClass(`active`);
+    }
+};
+const updateHeroImage = () => {
+    const scrollY = $(window).scrollTop();
+    const $heroSection = $(`#hero`);
+    const sectionY = $heroSection.offset().top - 64;
+    const imageHeight = $heroSection.height() - 31;
+    let percentage = (scrollY - sectionY) / imageHeight;
+    if (percentage < 0)
+        percentage = 0;
+    else if (percentage > 1)
+        percentage = 1;
+    $(`#hero-image`).css({ height: `${100 - percentage * 100}%` });
+    $(`#design-image`).css({ height: `${percentage * 100}%` });
+    let $designImg = $(`#design img`);
+    const shouldHideHero = scrollY > $designImg.offset().top - 31 - 64;
+    if (shouldHideHero && heroImageIsVisible) {
+        $designImg.css({ visibility: `visible` });
+        $(`#images-container`).css({ visibility: `hidden` });
+        heroImageIsVisible = false;
+    }
+    else if (!shouldHideHero && !heroImageIsVisible) {
+        $(`#images-container`).css({ visibility: `visible` });
+        $designImg.css({ visibility: `hidden` });
+        heroImageIsVisible = true;
+    }
+};
+const updateMaterialPositions = () => {
+    const scrollY = $(window).scrollTop();
+    const $materialsSection = $(`#materials`);
+    const sectionY = $materialsSection.offset().top;
+    const sectionHeight = $materialsSection.height();
+    let percentage = (scrollY - sectionY) / sectionHeight;
+    if (percentage < 0)
+        percentage = 0;
+    else if (percentage > 1)
+        percentage = 1;
+    $(`#rhodium`).css({ marginTop: percentage * 50 });
+    $(`#rosewood`).css({ marginTop: percentage * -50 });
 };
 main();
--- src/ts/scripts.ts
+++ src/ts/scripts.ts
@@ -2,11 +2,14 @@
 // src > ts > scripts.ts
 // Copyright © 2020 Ayden Gebran. All rights reserved.
 // Created by Ayden Gebran on 6 October 2020
-// Updated October 2020 (Version 1.0)
+// Updated 8 October 2020 (Version 1.0)
 
 // MODULES
 
 // CONSTANTS
+
+let activeSection = ``;
+let heroImageIsVisible = true;
 
 // VARIABLES
 
@@ -22,23 +25,66 @@ const setGlobalListeners = () => {
 
 const didScroll = () => {
 	updateActiveSection();
+	updateHeroImage();
+	updateMaterialPositions();
 };
 
 const updateActiveSection = () => {
 	const scrollY = $(window).scrollTop();
-	let activeSection = ``;
+	let section = ``;
 	if (scrollY > $(`#gallery`).offset().top)
-		activeSection = `gallery`;
+		section = `gallery`;
 	else if (scrollY > $(`#houses`).offset().top)
-		activeSection = `houses`;
+		section = `houses`;
 	else if (scrollY > $(`#materials`).offset().top)
-		activeSection = `materials`;
+		section = `materials`;
 	else if (scrollY > $(`#design`).offset().top)
-		activeSection = `design`;
-	$(`header nav li`).removeClass(`active`);
-	if (activeSection != ``)
-		$(`header nav li#${activeSection}-link`).addClass(`active`);
+		section = `design`;
+	if (section != activeSection) {
+		$(`header nav li`).removeClass(`active`);
+		if (section != ``)
+			$(`header nav li#${section}-link`).addClass(`active`);
+	}
 }
+
+const updateHeroImage = () => {
+	const scrollY = $(window).scrollTop();
+	const $heroSection = $(`#hero`);
+	const sectionY = $heroSection.offset().top - 64;
+	const imageHeight = $heroSection.height() - 31;
+	let percentage = (scrollY - sectionY) / imageHeight;
+	if (percentage < 0)
+		percentage = 0;
+	else if (percentage > 1)
+		percentage = 1;
+	$(`#hero-image`).css({ height: `${100 - percentage * 100}%` });
+	$(`#design-image`).css({ height: `${percentage * 100}%` });
+	let $designImg = $(`#design img`);
+	const shouldHideHero = scrollY > $designImg.offset().top - 31 - 64;
+	if (shouldHideHero && heroImageIsVisible) {
+		$designImg.css({ visibility: `visible` });
+		$(`#images-container`).css({ visibility: `hidden` });
+		heroImageIsVisible = false;
+	} else if (!shouldHideHero && !heroImageIsVisible) {
+		$(`#images-container`).css({ visibility: `visible` });
+		$designImg.css({ visibility: `hidden` });
+		heroImageIsVisible = true;
+	}
+}
+
+const updateMaterialPositions = () => {
+	const scrollY = $(window).scrollTop();
+	const $materialsSection = $(`#materials`);
+	const sectionY = $materialsSection.offset().top;
+	const sectionHeight = $materialsSection.height();
+	let percentage = (scrollY - sectionY) / sectionHeight;
+	if (percentage < 0)
+		percentage = 0;
+	else if (percentage > 1)
+		percentage = 1;
+	$(`#rhodium`).css({ marginTop: percentage * 50 });
+	$(`#rosewood`).css({ marginTop: percentage * -50 });
+};
 
 // INITIAL SCRIPTS
 
